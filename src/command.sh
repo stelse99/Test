f0eb48396f55746d2dd6a318df2a65cce36357d7
stelse99 <stelse99@gmail.com>
2022-02-19 19:50:40 +0900

    k8s yaml 적용

---
+++ src/command.sh
@@ -0,0 +1,14 @@
+kubeadm token list
+kubeadm token delete j4wtld.11b2vck3hpw9blb6
+kubeadm create token --ttl 50h
+kubeadm join 192.168.0.23:6443 --token j4wtld.11b2vck3hpw9blb6 \
+    --discovery-token-ca-cert-hash sha256:cbc842ce0289855f655b52a9ab8600bec8fbbd6711e0394c33688ee14fae275a
+
+
+kubectl scale statefulset ss-mainui --replicas=4
+kubectl scale statefulset ss-mainui --replicas=2
+
+kubectl edit statefulsets.apps ss-mainui
+kubectl describe statefulsets.apps ss-mainui
+kubectl rollout undo statefulset ss-mainui
+kubectl rollout history statefulset ss-mainui
